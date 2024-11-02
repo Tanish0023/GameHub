e4d0b619840ef652081641f49cd943af252ae865
Tanish0023 <tanish23aggarwal@gmail.com>
2024-11-02 18:17:14 +0530

    Sidebar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,4 +1,6 @@
+import { Container } from "./_components/container";
 import Navbar from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
 
 const BrowseLayout = ({
     children
@@ -7,7 +9,10 @@ const BrowseLayout = ({
         <>
             <Navbar/>
             <div className="h-full flex pt-20">
-                {children}
+                <Sidebar/>
+                <Container>
+                    {children}
+                </Container>
             </div> 
         </>
     );
