9a273cd1ca5d3e01b10b695d05e2235c88808443
Tanish0023 <tanish23aggarwal@gmail.com>
2024-11-09 13:28:27 +0530

    Livekit Webhook

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,12 +1,14 @@
 "use client"
 
-import { User } from "@prisma/client";
+import { Stream, User } from "@prisma/client";
 
 import { useSidebar } from "@/store/use-sidebar";
 import { UserItem, UserItemSkeleton } from "./user-Item";
 
 interface RecommendedProps{
-    data: User[]
+    data: (User & {
+        stream: Stream | null
+    })[]
 }
 
 const Recommended = ({data}: RecommendedProps) => {
@@ -28,7 +30,7 @@ const Recommended = ({data}: RecommendedProps) => {
                         key={user.id}
                         username={user.username}
                         imageUrl={user.imageUrl}
-                        isLive={true}
+                        isLive={user.stream?.isLive}
                     />
                 ))}
             </ul>
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -45,6 +45,9 @@ export const getRecommended = async () => {
                     }
                 ]
             },
+            include:{
+                stream: true
+            },
             orderBy:{
                 createdAt: "desc"
 
@@ -53,6 +56,9 @@ export const getRecommended = async () => {
 
     }else{
         users = await db.user.findMany({
+            include:{
+                stream:true
+            },
             orderBy:{
                 createdAt: "desc"
             }
