e4b7eb20276272c081940d6bfcbf624b381f0708
Tanish0023 <tanish23aggarwal@gmail.com>
2024-11-16 12:14:19 +0530

    Deployment Try 1

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,12 @@ const nextConfig: NextConfig = {
       "utfs.io"
     ]
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  runtime: 'nodejs'
 };
 
 export default nextConfig;
